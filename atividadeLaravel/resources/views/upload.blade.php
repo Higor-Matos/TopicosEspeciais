081a848080e44369a1bb0b0b7d1435d4a7ba7af5
Higor de Deus <higordeus22@gmail.com>
2022-11-14 12:27:00 -0300

    "AGORA SIM"

--- atividadeLaravel/resources/views/upload.blade.php
+++ atividadeLaravel/resources/views/upload.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> Document </title>
+    <title> Documentos </title>
 </head> 
 <body>
     <h1>Upload</h1>
     <form method="POST" action="/upload" enctype="multipart/form-data">
     @csrf
-    <input type="file" name="image">
+    <input type="file" name="Arquivo">
     <input type="submit" name="Upload">
     </form>
 </body>
